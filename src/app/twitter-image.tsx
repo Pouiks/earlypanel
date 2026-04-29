9bf7364916f656557c6b3992aa678aff18133b09
Pouiks <virgilejoinville@gmail.com>
2026-04-29 21:32:45 +0200

    OG resolution 2

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -1,4 +1,7 @@
 // Reutilise la meme image OG pour Twitter/X.
 // Next 16 file convention : ce fichier produit /twitter-image et est
 // automatiquement injecte dans <meta name="twitter:image">.
-export { default, runtime, alt, size, contentType } from "./opengraph-image";
+// `runtime` doit etre declare localement (Next 16 / Turbopack interdit
+// le reexport pour les segment options).
+export const runtime = "edge";
+export { default, alt, size, contentType } from "./opengraph-image";
